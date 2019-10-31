c4431d36774e47348ac0d300cc69d4a4d3d7b2eb
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-31 12:49:17 -0500

    added MSG

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 
 const server = express(); // creating an instance of an express server
 
 server.get('/', (req, res) => {
-  res.status(200).json({ hello: 'Web 23' });
+  res.status(200).json({ message: process.env.msg });
 });
 
 
@@ -13,5 +14,5 @@ const port = process.env.port || 4000;
 //in this case, the port will be set by heroku when we deploy
 
 server.listen(port, () => {
-  console.log(`\n Server running on port ${4000} \n`);
+  console.log(`\n Server running on port ${port} \n`);
 });
