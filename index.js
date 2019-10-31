1289008d5246e61fa8e0049fe89c7a9506c5270e
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-31 12:22:47 -0500

    deploy

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ server.get('/', (req, res) => {
   res.status(200).json({ hello: 'Web 23' });
 });
 
-server.listen(4000, () => {
+
+// the port is coming from the environment
+const port = process.env.port || 4000;
+
+//in this case, the port will be set by heroku when we deploy
+
+server.listen(port, () => {
   console.log('\n Server running on port 4000 \n');
 });
